a5d69790bcdea8d23fed527e34d143d456486186
moukil01 <moukil01@gmail.com>
2018-11-12 18:37:08 +0100

    quetes POO3

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public  class Eagle extends Bird implements FLy {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,49 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
-    }
+    };
+    @Override
+     public  void takeOff(){
+    	if(!this.flying) {
+    		this.flying = true;
+    	}
+    	 System.out.println ("vers l'infini est au delas");
+     };
+     @Override
+     public int ascend(int meters) {
+         if (this.flying) {
+             this.altitude = Math.min(this.altitude + meters, 150);
+             System.out.printf(this.getName() +"%s flying downward, altitude : %d%n", this.getName(), this.altitude);
+         }
+         return this.altitude;
+     }
+     
+     @Override
+     public int descend(int meters) {
+         if (this.flying) {
+             this.altitude = Math.min(this.altitude - meters, 10);
+             System.out.printf("%s flying downward, altitude : %d%n", this.getName(), this.altitude);
+         }
+         return this.altitude;
+     }
+     
+     @Override
+     public void land() {
+         if (this.flying && this.altitude <= 1) {
+        	 this.altitude = 0;
+             System.out.printf("%s lands on the ground.%n", this.getName());
+         } else {
+             System.out.printf("%s is too high, it can't lands.%n", this.getName());
+         }
+     }
+
+//     $ Eye Cherry takes off in the sky.
+//     $ Eye Cherry flies upward, altitude : 120
+//     $ Eye Cherry flies upward, altitude : 150//     $ It glides into the air.
+//     $ Eye Cherry flies downward, altitude : 10
+//     $ Eye Cherry is too high, it can't lands.
+//     $ Eye Cherry flies downward, altitude : 1
+//     $ Eye Cherry lands on the ground.
+     
+  
 }
--- Nature.java
+++ Nature.java
@@ -2,18 +2,11 @@ public class Nature {
 
     public static void main(String[] args) {
 
-        Penguin pingou = new Penguin("Pingou");
-        pingou.dive();
-        pingou.swimDown(3);
-        pingou.swimUp(1);
-        pingou.swimDown(4);
-        pingou.swimUp(5);
-        pingou.swimUp(1);
-        pingou.getOut();
+     
 
         Eagle eyeCherry = new Eagle("Eye Cherry");
-        // TODO : décommente le code suivant afin de le tester
-        /*
+        //TODO : décommente le code suivant afin de le tester
+        
         eyeCherry.takeOff();
         eyeCherry.ascend(120);
         eyeCherry.ascend(30);
@@ -22,6 +15,6 @@ public class Nature {
         eyeCherry.land();
         eyeCherry.descend(9);
         eyeCherry.land();
-        */
+        
     }
 }
